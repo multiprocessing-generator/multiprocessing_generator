4da0457c0e54d3d349b466143fb06f6c611760ab
Antonin Delpeuch <antonin@delpeuch.eu>
2017-05-29 11:58:58 +0200

    Update version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1',
+    version='0.2',
 
     description='Prefetch elements from a Python generator in the background, from a separate process',
     long_description=long_description,
@@ -55,6 +55,8 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
 
     # What does your project relate to?
