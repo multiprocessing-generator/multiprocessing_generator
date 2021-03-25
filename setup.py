4cf6c0f0183a2e4cfac371a9b18dc345ae27b4ad
Antonin Delpeuch <antonin@delpeuch.eu>
2021-03-25 12:16:09 +0100

    Release 0.3

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2',
+    version='0.3',
 
     description='Prefetch elements from a Python generator in the background, from a separate process',
     long_description=long_description,
